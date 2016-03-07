2b19a21ad358f86e29cd38783cb24d2b6a9a5141
Wojciech Pierscionek <PierscionekW@gmail.com>
2016-03-06 23:52:48 -0400

    update

--- homework/assets/javascript/file.js
+++ homework/assets/javascript/file.js
@@ -7,17 +7,14 @@ function gernerateButtons() {
 
 	for (var i = 0; i < cartoonsList.length; i++) { 
 	var cartoonButton = $("<button id="+cartoonsList[i]+">"+cartoonsList[i]+"</button>");
-	// console.log (cartoonButton);
 	cartoonButton.attr("data-name", cartoonsList[i]);
 	$("#cartoons").append(cartoonButton);
 	}
 };
-
-gernerateButtons();
-
-$("button").on('click', function() {
-        var button  = $(this).data('name');
-        var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + button + "&api_key=dc6zaTOxFJmzC&limit=10";
+	
+	function displayCartoonInfo() {
+        var button  = $(this).attr('data-name');
+        var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + button + "&api_key=dc6zaTOxFJmzC&limit=21";
 
         $.ajax({
                 url: queryURL,
@@ -31,19 +28,25 @@ $("button").on('click', function() {
                     var rating = results[i].rating;
                     var button = $('<p>').text("Rating: " + rating);
                     var personImage = $('<img>');
-                    personImage.attr('src', results[i].images.fixed_height.url);
+                    personImage.attr('src', results[i].images.fixed_height_small.url);
 
                     gifContainer.append(button)
                     gifContainer.append(personImage)
                     $('#gifs').prepend(gifContainer);
                 }
-
             });
-        });
+        };
+ gernerateButtons(); 
 
 $('#search').on('click', function(){
-    var cartoon = $('#cartoon-input').val().trim();
-    cartoonsList.push(cartoon);
+
+    var userInput = $('#cartoon-input').val().trim();
+    cartoonsList.push(userInput);
     gernerateButtons();
      return false;
   });
+$("#clear").on("click", function() {
+	$("#gifs").empty();
+});
+$(document).on('click', 'button', displayCartoonInfo);
+
