b98c4cbd6c6ae4d5b9147768b4d8d8f7d0dfc8fa
Wojciech Pierscionek <PierscionekW@gmail.com>
2016-03-07 20:39:03 -0400

    3/7/2016

--- homework/assets/javascript/file.js
+++ homework/assets/javascript/file.js
@@ -1,5 +1,5 @@
 var cartoonsList = ["Tom And Jerry","Full Metall Alchemist", "One Piece", "Dragon Ball Z",
- "Naruto", "Ed, Edd, 'n' Eddy", "Courage the Cowardly Dog", "Dexter's Laboratory ",
+ "Naruto", "Ed, Edd, n Eddy", "Courage the Cowardly Dog", "Dexter's Laboratory ",
   "The Smurfs", "Looney Tunes", "Inspector Gadget", "X-men", "Garfield and Friends"]; 
 
 function gernerateButtons() {
@@ -9,38 +9,45 @@ function gernerateButtons() {
 	var cartoonButton = $("<button id="+cartoonsList[i]+">"+cartoonsList[i]+"</button>");
 	cartoonButton.attr("data-name", cartoonsList[i]);
 	$("#cartoons").append(cartoonButton);
-	}
+	};
 };
 	
 	function displayCartoonInfo() {
-        var button  = $(this).attr('data-name');
+        var button  = $(this).attr("data-name");
         var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + button + "&api_key=dc6zaTOxFJmzC&limit=21";
 
         $.ajax({
                 url: queryURL,
-                method: 'GET'
+                method: "GET"
             })
             .done(function(response) {
                 var results = response.data;
 
                 for (var i = 0; i < results.length; i++) {
-                    var gifContainer = $('<div class="item">')
+                    var gifContainer = $("<div class=item>")
                     var rating = results[i].rating;
-                    var button = $('<p>').text("Rating: " + rating);
-                    var personImage = $('<img>');
-                    personImage.attr('src', results[i].images.fixed_height_small.url);
+                    var button = $("<p>").text("Rating:" + rating);
 
+                    var personImage = $("<img>");
+                    personImage.addClass("personImageClass");
+                    personImage.attr("src", results[i].images.fixed_height.url);
+                    personImage.attr("data-still", results[i].images.fixed_height_still.url);
+    				personImage.attr("data-animate", results[i].images.fixed_height.url);
+    				personImage.attr("data-state", "animate");
                     gifContainer.append(button)
                     gifContainer.append(personImage)
-                    $('#gifs').prepend(gifContainer);
-                }
+                    $("#gifs").prepend(gifContainer);
+                };
+                if (results.length == 0) {
+                	$("#cartoons").append("<p> There is no gifs for this title.</p>");
+                };
             });
         };
  gernerateButtons(); 
 
-$('#search').on('click', function(){
+$("#search").on("click", function() {
 
-    var userInput = $('#cartoon-input').val().trim();
+    var userInput = $("#cartoon-input").val().trim();
     cartoonsList.push(userInput);
     gernerateButtons();
      return false;
@@ -48,5 +55,16 @@ $('#search').on('click', function(){
 $("#clear").on("click", function() {
 	$("#gifs").empty();
 });
-$(document).on('click', 'button', displayCartoonInfo);
 
+ $("#gifs").on("click", ".personImageClass", function() { 
+  	var state = $(this).attr("data-state"); 
+  	if (state == "still"){
+                $(this).attr("src", $(this).data("animate"));
+                $(this).attr("data-state", "animate");
+            }else{
+                $(this).attr("src", $(this).data("still"));
+                $(this).attr("data-state", "still");
+            };
+	});
+
+$(document).on("click", "button", displayCartoonInfo);
